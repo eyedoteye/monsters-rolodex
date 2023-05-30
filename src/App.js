e29a574a1fdf51cb9c1b8d2417a6a7c9191fb3b6
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2023-05-30 15:21:44 -0400

    remove callback from setState in fetch

--- src/App.js
+++ src/App.js
@@ -20,12 +20,9 @@ class App extends Component {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => console.log(this.state)
-        )
+        this.setState(() => {
+          return { monsters: users };
+        })
       );
   }
 
