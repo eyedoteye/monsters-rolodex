f87c4441fe4b87584337eec7f9a93e83a81a8498
Krishnan Sarkar <sarkar.krishnan@gmail.com>
2023-05-30 19:23:13 -0400

    change searchbox to functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,14 +4,12 @@ import Card from '../card/card.component';
 
 import './card-list.styles.css';
 
-const CardList = ({ monsters }) => {
-  return (
-    <div className="card-list">
-      {monsters.map(monster => {
-        return <Card monster={monster} key={monster.id} />;
-      })}
-    </div>
-  );
-};
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map(monster => {
+      return <Card monster={monster} key={monster.id} />;
+    })}
+  </div>
+);
 
 export default CardList;
